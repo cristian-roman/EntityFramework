248a56513cf244c739d5389dd699cd17e9b739ae
Cristian Roman <romancristianroman@yahoo.com>
2022-07-08 16:30:53 +0300

    [CHANGE]: Huge Battle class Changes

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -11,36 +11,7 @@ namespace SamuraiApp
         private static SamuraiContext _context = new SamuraiContext();
         public static void Main(string[] args)
         {
-            AddSamurais("Okomoto", "Shimada", "Kikuchio", "Hayashida", "Cristi");
-            //AddVariousTypes();
-            GetSamurais();
-        }
-
-        private static void AddVariousTypes()
-        {
-            _context.AddRange(new Samurai{Name = "Shimada"},
-                                        new Samurai{Name = "Okomoto"},
-                                        new Battle{Name="Battle of Anegawa"},
-                                        new Battle{Name = "Battle of Nagashino"});
-            _context.SaveChanges();
-        }
-
-        private static void AddSamurais(params string[] names)
-        {
-            foreach (string name in names)
-            {
-                _context.Samurais.Add(new Samurai() { Name = name });
-            }
-
-            _context.SaveChanges();
-        }
-
-        private static void GetSamurais()
-        {
-            var samurais = _context.Samurais
-                .TagWith("ConsoleApp.Program.GetSamurais method")
-                .ToList();
-            Console.WriteLine($"Samurai count is {samurais.Count}");
+          
         }
     }
 }

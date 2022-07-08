18431eac97ac2b6a5b5151076cac0226e8aa86f2
Cristian Roman <romancristianroman@yahoo.com>
2022-07-08 12:31:23 +0300

    [Database] updateFields

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,17 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using SamuraiApp.Domain;
 
-namespace SamuraiApp.Data;
-
-public class SamuraiContext : DbContext
+namespace SamuraiApp.Data
 {
-    public DbSet<Samurai> Samurais {get; set; }
-    
-    public DbSet<Quote> Quotes { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public partial class SamuraiContext : DbContext
     {
-        optionsBuilder.UseSqlServer(
-            "Data Source = (localdb) \\ MSSQLLocalDB; Initial catalog=SamuraiAppData");
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public SamuraiContext()
+        {
+        }
+
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=bazaMeaDeDate;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
